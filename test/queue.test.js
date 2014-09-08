ad389a4da7fb065d07ff4a3d7d34f70d2c0f78b0
Hunter Loftis <hunter@hunterloftis.com>
2014-09-08 17:26:09 +0100

    more queue testing

--- test/queue.test.js
+++ test/queue.test.js
@@ -3,40 +3,101 @@ var jackrabbit = require('..');
 var Queue = require('../lib/queue');
 
 var RABBIT_URL = process.env.RABBIT_URL || 'amqp://localhost';
+var QUEUE_NAME = 'test.queue.' + Math.round(Math.random() * 100000);
 
 describe('jackrabbit', function() {
 
-  describe('Queues', function() {
-    describe('jackrabbit#queue', function() {
+  describe('#queue', function() {
 
-      beforeEach(function connect(done) {
-        this.broker = jackrabbit(RABBIT_URL);
-        this.broker.once('connected', done);
+    beforeEach(function connect(done) {
+      this.broker = jackrabbit(RABBIT_URL);
+      this.broker.once('connected', done);
+    });
+
+    describe('with a queue name', function() {
+
+      it('returns a new Queue instance', function() {
+        var queue = this.broker.queue(QUEUE_NAME);
+        assert.instanceOf(queue, Queue);
       });
 
-      describe('with a queue name', function() {
+      it('emits ready when the queue has been asserted', function(done) {
+        var queue = this.broker.queue(QUEUE_NAME);
+        queue.on('ready', done);
+      });
 
-        it('returns a new Queue instance', function() {
-          var queue = this.broker.queue('test.queue');
-          assert.instanceOf(queue, Queue);
+      it('defaults to a durable queue', function() {
+        var queue = this.broker.queue(QUEUE_NAME);
+        assert.equal(queue.durable, true);
+      });
+
+      it('defaults to a prefetch of 1', function() {
+        var queue = this.broker.queue(QUEUE_NAME);
+        assert.equal(queue.prefetch, 1);
+      });
+    });
+
+    describe('with durability disabled in options', function() {
+
+      it('returns a non-durable queue', function() {
+        var queue = this.broker.queue(QUEUE_NAME + '.durability', { durable: false });
+        assert.equal(queue.durable, false);
+      });
+    });
+
+    describe('with prefetch of 5 in options', function() {
+
+      before(function createQueue(done) {
+        this.queue = this.broker.queue(QUEUE_NAME + '.prefetch', { prefetch: 5 });
+        this.queue.on('ready', done);
+      });
+
+      before(function publishTen() {
+        var i = 10;
+        while (i--) this.queue.publish({ remaining: i });
+      });
+
+      it('returns a queue with prefetch of 5', function() {
+        assert.equal(this.queue.prefetch, 5);
+      });
+
+      it('fetches 5 messages before pausing', function(done) {
+        var i = 0;
+        setTimeout(function checkFetched() {
+          assert.equal(i, 5);
+          done();
+        }, 50);
+        this.queue.subscribe(function handler(msg, acknowledge) {
+          i++;
+          assert.equal(msg.remaining, 10 - i);
+          if (i > 5) throw new Error('Prefetched more than 5');
         });
+      });
+    });
 
-        it('emits ready when the queue has been asserted', function(done) {
-          var queue = this.broker.queue('test.queue');
-          queue.on('ready', done);
+    describe('with conflicting options', function() {
+
+      before(function(done) {
+        var queue = this.broker.queue(QUEUE_NAME + '.conflicting', { durable: false });
+        queue.on('ready', done);
+      });
+
+      it('emits an error', function(done) {
+        var queue = this.broker.queue(QUEUE_NAME + '.conflicting', { durable: true });
+        queue.on('error', function(err) {
+          assert.ok(err);
+          done();
         });
+      });
 
-        it('defaults to a durable queue', function() {
-          var queue = this.broker.queue('test.queue');
-          assert.equal(queue.durable, true);
-        });
-
-        it('defaults to a prefetch of 1', function() {
-          var queue = this.broker.queue('test.queue');
-          assert.equal(queue.prefetch, 1);
+      it("doesn't emit ready", function(done) {
+        setTimeout(done, 50);
+        var queue = this.broker.queue(QUEUE_NAME + '.conflicting', { durable: true });
+        queue.on('error', function() {});
+        queue.on('ready', function() {
+          throw new Error("Shouldn't emit ready");
         });
       });
     });
   });
-
 });
