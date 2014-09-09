c50e3032099e3fa6acab82cfdfc37614188cda47
Hunter Loftis <hunter@hunterloftis.com>
2014-09-09 15:04:46 +0100

    progress

--- test/queue.test.js
+++ test/queue.test.js
@@ -1,9 +1,7 @@
 var assert = require('chai').assert;
 var jackrabbit = require('..');
 var Queue = require('../lib/queue');
-
-var RABBIT_URL = process.env.RABBIT_URL || 'amqp://localhost';
-var QUEUE_NAME = 'test.queue.' + Math.round(Math.random() * 100000);
+var util = require('./util');
 
 describe('jackrabbit', function() {
 
@@ -12,19 +10,19 @@ describe('jackrabbit', function() {
     describe('with a queue name', function() {
 
       before(function connect(done) {
-        this.queue = jackrabbit(RABBIT_URL);
+        this.queue = jackrabbit(util.RABBIT_URL);
         this.queue.once('connected', done);
       });
 
       before(function create(done) {
-        this.name = QUEUE_NAME;
+        this.name = util.NAME;
         this.queue.create(this.name, function(err, instance) {
           this.instance = instance;
           done(err);
         }.bind(this));
       });
 
-      after(function(done) {
+      after(function cleanup(done) {
         this.queue.destroy(this.name, done);
       });
 
@@ -36,24 +34,21 @@ describe('jackrabbit', function() {
         assert.equal(this.instance.durable, true);
       });
 
-      it('defaults to a prefetch of 1', function() {
-        assert.equal(this.instance.prefetch, 1);
-      });
     });
 
     describe('with durability disabled in options', function() {
 
       before(function connect(done) {
-        this.queue = jackrabbit(RABBIT_URL);
+        this.queue = jackrabbit(util.RABBIT_URL);
         this.queue.once('connected', done);
       });
 
       before(function create(done) {
-        this.name = QUEUE_NAME + '.durability';
+        this.name = util.NAME + '.durability';
         this.queue.create(this.name, { durable: false }, function(err, instance) {
           this.instance = instance;
           done(err);
-        });
+        }.bind(this));
       });
 
       after(function(done) {
@@ -65,97 +60,24 @@ describe('jackrabbit', function() {
       });
     });
 
-    describe('in a connection with prefetch 1', function() {
-
-      before(function connect(done) {
-        this.queue = jackrabbit(RABBIT_URL, 1);
-        this.queue.once('connected', done);
-      });
-
-      before(function createQueue(done) {
-        this.name = QUEUE_NAME + '.prefetch';
-        this.queue.create(this.name, done);
-      });
-
-      before(function publishTen() {
-        var i = 10;
-        while (i--) this.queue.publish(this.name, { remaining: i });
-      });
-
-      after(function(done) {
-        this.queue.destroy(this.name, done);
-      });
-
-      it('knows to prefetch 1 message', function() {
-        assert.equal(this.queue.prefetch, 1);
-      });
-
-      it('fetches 1 messages before pausing', function(done) {
-        var i = 0;
-        setTimeout(function checkFetched() {
-          assert.equal(i, 1);
-          done();
-        }, 50);
-        this.queue.handle(this.name, function handler(msg, acknowledge) {
-          i++;
-          assert.equal(msg.remaining, 10 - i);
-          if (i > 1) throw new Error('Prefetched more than 1');
-        });
-      });
-    });
-
-    describe('in a connection with prefetch 5', function() {
-
-      before(function connect(done) {
-        this.queue = jackrabbit(RABBIT_URL, 5);
-        this.queue.once('connected', done);
-      });
-
-      before(function createQueue(done) {
-        this.name = QUEUE_NAME + '.prefetch';
-        this.queue.create(this.name, done);
-      });
-
-      before(function publishTen() {
-        var i = 10;
-        while (i--) this.queue.publish(this.name, { remaining: i });
-      });
-
-      after(function(done) {
-        this.queue.destroy(this.name, done);
-      });
-
-      it('knows to prefetch 5 messages', function() {
-        assert.equal(this.queue.prefetch, 5);
-      });
-
-      it('prefetches 5 messages in order', function(done) {
-        var i = 0;
-        setTimeout(function checkFetched() {
-          assert.equal(i, 5);
-          done();
-        }, 50);
-        this.queue.handle(this.name, function handler(msg, acknowledge) {
-          i++;
-          assert.equal(msg.remaining, 10 - i);
-          if (i > 5) throw new Error('Prefetched more than 5');
-        });
-      });
-    });
-
     describe('with conflicting options', function() {
 
       before(function connect(done) {
-        this.queue = jackrabbit(RABBIT_URL);
+        this.queue = jackrabbit(util.RABBIT_URL);
         this.queue.once('connected', done);
       });
 
       before(function createQueue(done) {
-        this.name = QUEUE_NAME + '.conflicting';
+        this.name = util.NAME + '.conflicting';
         this.queue.create(this.name, { durable: false }, done);
       });
 
-      after(function(done) {
+      after(function reconnect(done) {
+        this.queue = jackrabbit(util.RABBIT_URL);
+        this.queue.once('connected', done);
+      });
+
+      after(function cleanup(done) {
         this.queue.destroy(this.name, done);
       });
 
@@ -172,14 +94,14 @@ describe('jackrabbit', function() {
   describe('#destroy', function() {
 
     before(function connect(done) {
-      this.queue = jackrabbit(RABBIT_URL);
+      this.queue = jackrabbit(util.RABBIT_URL);
       this.queue.once('connected', done);
     });
 
     describe('with a queue that exists', function() {
 
       it('needs a queue', function(done) {
-        this.name = QUEUE_NAME + '.destroy';
+        this.name = util.NAME + '.destroy';
         this.queue.create(this.name, { durable: true }, done);
       });
 
@@ -221,7 +143,7 @@ describe('jackrabbit', function() {
   });
 
   after(function connect(done) {
-    this.broker = jackrabbit(RABBIT_URL);
+    this.broker = jackrabbit(util.RABBIT_URL);
     this.broker.once('connected', done);
   });
 
