a0ecc2790ffe2929e34874068e63844d00048045
Hunter Loftis <hunter@hunterloftis.com>
2015-09-14 01:31:24 -0400

    works with example 6, rpc

--- examples/6-rpc/client.js
+++ examples/6-rpc/client.js
@@ -1,14 +1,15 @@
-var jackrabbit = require('jackrabbit');
+var jackrabbit = require('../..');
 
-var rabbit = jackrabbit(RABBIT_URL);
+var rabbit = jackrabbit(process.env.RABBIT_URL);
 var exchange = rabbit.default();
-var rpc = exchange.queue({ name: 'rpc_queue', prefetch: 1 });
+var rpc = exchange.queue({ name: 'rpc_queue', prefetch: 1, durable: false });
 
-exchange.publish({ n: 123 }, {
+exchange.publish({ n: 40 }, {
   key: 'rpc_queue',
   reply: onReply    // auto sends necessary info so the reply can come to the exclusive reply-to queue for this rabbit instance
 });
 
 function onReply(data) {
   console.log('result:', data.result);
+  rabbit.close();
 }
--- examples/6-rpc/server.js
+++ examples/6-rpc/server.js
@@ -1,12 +1,13 @@
-var jackrabbit = require('jackrabbit');
+var jackrabbit = require('../..');
 
-var rabbit = jackrabbit(RABBIT_URL);
+var rabbit = jackrabbit(process.env.RABBIT_URL);
 var exchange = rabbit.default();
-var rpc = exchange.queue({ name: 'rpc_queue', prefetch: 1 });
+var rpc = exchange.queue({ name: 'rpc_queue', prefetch: 1, durable: false });
 
 rpc.consume(onRequest);
 
 function onRequest(data, reply) {
+  console.log('got request for n:', data.n);
   reply({ result: fib(data.n) });
 }
 
