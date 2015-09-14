fbe497d21b6d3444affd4653144036b9cb2fdce9
Hunter Loftis <hunter@hunterloftis.com>
2015-09-13 20:59:51 -0400

    example 2 working

---
+++ examples/2-work-queues/spanish.js
@@ -0,0 +1,12 @@
+var jackrabbit = require('../..');
+
+var rabbit = jackrabbit(process.env.RABBIT_URL);
+var exchange = rabbit.direct();
+var hello = exchange.queue({ name: 'task_queue', durable: true });
+
+hello.consume(onGreet);
+
+function onGreet(data, ack) {
+  console.log('Hola, ' + data.name + '!');
+  ack();
+}
--- examples/work/publisher.js
+++
@@ -1,7 +0,0 @@
-var jackrabbit = require('jackrabbit');
-
-var rabbit = jackrabbit(RABBIT_URL);
-var exchange = rabbit.default();
-var hello = exchange.queue({ name: 'jobs.hello', durable: true });
-
-exchange.publish({ name: 'Hunter' }, { key: 'jobs.hello' });
