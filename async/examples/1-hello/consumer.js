4f554c307322f013b318c12ce4b3baf3f2e235fc
Hunter Loftis <hunter@hunterloftis.com>
2017-03-15 22:31:46 -0400

    producing and consuming

--- async/examples/1-hello/consumer.js
+++ async/examples/1-hello/consumer.js
@@ -1,6 +1,12 @@
-const jackrabbit = require('../..')
-const queue = await jackrabbit(process.env.RABBIT_URL).queue({ name: 'hello' })
+const jackrabbit = require('../../jackrabbit')
+const RABBIT_URL = process.env.CLOUDAMQP_URL
 
-queue.on('message', async (data) => {
-  console.log('received', data)
-})
+main()
+
+async function main() {
+  const queue = await jackrabbit(RABBIT_URL).queue({ name: 'hello' })
+
+  queue.on('message', async (data) => {
+    console.log('received', data)
+  })
+}
--- async/jackrabbit.js
+++ async/jackrabbit.js
@@ -8,30 +8,68 @@ const DEFAULT_EXCHANGES = {
 }
 
 module.exports = (url) => {
+  const actives = new Set()
   let connection
-  const exchanges = new Set()
 
   return { exchange, queue }
 
   async function exchange(name = '', type = 'direct', options) {
     connection = connection || await amqp.connect(url)
     const newExchange = await Exchange(connection, name, type, options)
-    exchanges.add(newExchange)
-    newExchange.once('close', removeExchange)
+    actives.add(newExchange)
+    newExchange.once('close', removeActive)
     return newExchange
   }
 
-  async function queue() {
-
+  async function queue(options) {
+    connection = connection || await amqp.connect(url)
+    const newQueue = await Queue(connection, options)
+    actives.add(newQueue)
+    newQueue.once('close', removeActive)
+    return newQueue
   }
 
-  async function removeExchange(oldExchange) {
-    exchanges.delete(oldExchange)
-    console.log('new exchange size:', exchanges.size)
-    if (exchanges.size === 0) {
+  async function removeActive(item) {
+    if (!actives.delete(item)) {
+      throw new Error('Tried to remove a nonexistant Exchange or Queue')
+    }
+    if (actives.size === 0) {
       await connection.close()
       connection = undefined
     }
+  }
+}
+
+async function Queue(connection, options) {
+  const instance = Object.assign(new EventEmitter(), { })
+  const channel = await connection.createChannel()
+  const queue = await channel.assertQueue(options.name, options)
+  const name = queue.queue
+  let consumerTag
+
+  instance
+    .on('newListener', consume)
+    .on('removeListener', cancel)
+    .emit('connect')
+
+  return instance
+
+  async function consume(event, listener) {
+    if (event !== 'message') return
+    if (consumerTag) return
+    consumerTag = await channel.consume(name, onMessage)
+  }
+
+  async function cancel(event, listener) {
+    if (event !== 'message') return
+    if (!consumerTag) return
+    if (emitter.listenerCount('message') > 0) return
+    await channel.cancel(consumerTag)
+    consumerTag = undefined
+  }
+
+  function onMessage(msg) {
+    instance.emit('message', msg.content.toString(), msg)
   }
 }
 
@@ -41,6 +79,7 @@ async function Exchange(connection, name, type, options) {
   const isDefault = (name === '' || DEFAULT_EXCHANGES[type] === name)
 
   if (!isDefault) await channel.assertExchange(name, type, options)
+  instance.emit('connect')
   connection.on('close', () => instance.emit('disconnect'))
   channel.on('close', () => instance.emit('close', instance))
   channel.on('drain', () => instance.emit('drain'))
