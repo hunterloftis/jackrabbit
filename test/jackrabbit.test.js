4fb9816da521f9180fa607ca7166240b676bb87d
Hunter Loftis <hunter@hunterloftis.com>
2017-03-17 21:59:59 -0400

    tests for exchange configuration

--- test/jackrabbit.test.js
+++ test/jackrabbit.test.js
@@ -2,7 +2,7 @@ const jackrabbit = require('..')
 const pevent = require('promisify-event')
 const RABBIT_URL = process.env.CLOUDAMQP_URL
 
-console.log = () => {}
+// console.log = () => {}
 
 describe('broker', () => {
   const url = 'amqp://localhost/foo'
@@ -44,11 +44,51 @@ describe('.exchange', () => {
         expect(err.message).toMatch(/ENOTFOUND/)
       }
     })
-    it('exposes publish, close, and queue', async () => {
+    it('exposes publish, close, queue, and config', async () => {
       let exchange = await broker.exchange()
       expect(exchange).toHaveProperty('publish')
       expect(exchange).toHaveProperty('close')
       expect(exchange).toHaveProperty('queue')
+      expect(exchange).toHaveProperty('config')
+      await exchange.close()
     })
+
+  })
+  describe('publish', async () => {
+    it("uses the default exchange ('') by default", async () => {
+      const exchange = await jackrabbit(RABBIT_URL).exchange()
+      expect(exchange.config.name).toBe('')
+      await exchange.close()
+    })
+    it('can create a named exchange', async () => {
+      const exchange = await jackrabbit(RABBIT_URL).exchange({ name: 'foobar' })
+      expect(exchange.config.name).toBe('foobar')
+      await exchange.close()
+    })
+    it('uses immediate mode by default', async () => {
+      const exchange = await jackrabbit(RABBIT_URL).exchange()
+      expect(exchange.config.mode).toBe('immediate')
+      await exchange.close()
+    })
+    it('can use confirm mode', async () => {
+      const exchange = await jackrabbit(RABBIT_URL).exchange({ mode: 'confirm' })
+      expect(exchange.config.mode).toBe('confirm')
+      await exchange.close()
+    })
+    it('can use reply mode', async () => {
+      const exchange = await jackrabbit(RABBIT_URL).exchange({ mode: 'reply' })
+      expect(exchange.config.mode).toBe('reply')
+      await exchange.close()
+    })
+    it('throws an error if a non-supported mode is used', async () => {
+      expect.assertions(1)
+      try {
+        const exchange = await jackrabbit(RABBIT_URL).exchange({ mode: 'foobar' })
+      }
+      catch (err) {
+        expect(err.message).toMatch(/No publish method for mode/)
+      }
+    })
+
   })
 })
