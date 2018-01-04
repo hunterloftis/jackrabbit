af59199929b6b3c5d0151d6bf884bbd44b8231a1
Fatih Aydın <fatih.aydin@emlakjet.com>
2018-01-05 02:02:59 +0300

    Fix missing semicolon

--- test/env.js
+++ test/env.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
 // Loaded by Mocha. Enforces 'test' as the node environment.
 
-process.env.NODE_ENV = 'test'
-process.env.RABBIT_URL = 'amqp://localhost'
+process.env.NODE_ENV = 'test';
+process.env.RABBIT_URL = 'amqp://localhost';
